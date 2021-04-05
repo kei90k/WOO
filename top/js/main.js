2b590fe323d7e74491ebd296e5087221cdba91b1
kei90k <nake99psk@yahoo.ne.jp>
2021-04-05 18:00:37 +0900

    update

--- top/js/main.js
+++ top/js/main.js
@@ -28,6 +28,7 @@ $(function(){
             $('.side-btn01 > p').css('color', '#707070');
              $('.side-btn02').css('border-color', '#707070');
              $('.side-btn02 > p').css('color', '#707070');
+
          }
          else if(ssawAreaHeight - halfHeight < dy && newitemAreaHeight - halfHeight > dy)
          {
@@ -35,6 +36,7 @@ $(function(){
             $('.side-btn01 > p').css('color', '#f4f4f4');
              $('.side-btn02').css('border-color', '#f4f4f4');
              $('.side-btn02 > p').css('color', '#f4f4f4');
+
          }
          else if(newitemAreaHeight - halfHeight < dy)
          {
@@ -42,6 +44,7 @@ $(function(){
             $('.side-btn01 > p').css('color', '#707070');
              $('.side-btn02').css('border-color', '#707070');
              $('.side-btn02 > p').css('color', '#707070');
+
          }
          else
          {
@@ -55,18 +58,94 @@ $(function(){
          if(aboutAreaHeight - humHeight < dy && ssawAreaHeight - humHeight > dy)
          {
             $('header .menu-button span').css('background-color', '#707070');
+            $('header .icon').css('color', '#707070');
          }
          else if(ssawAreaHeight - humHeight < dy && newitemAreaHeight - humHeight > dy)
          {
             $('header .menu-button span').css('background-color', '#f4f4f4');
+            $('header .icon').css('color', '#f4f4f4');
          }
          else if(newitemAreaHeight - humHeight < dy)
          {
-            $('header .menu-button span').css('background-color', '#707070');
+            $('header .menu-button span').css('background-color', '#707070');$('header .icon').css('color', '#707070');
          }
          else
          {
             $('header .menu-button span').css('background-color', '#f4f4f4');
+            $('header .icon').css('color', '#f4f4f4');
          }
      });
+
+   //   ____________パララックス_________
+
+
+
+
+
+   // 1. headerタグの高さ取得(変数headerHeight)
+   var aboutAreaHeight= $('.box1').offset().top;
+   console.log('about画像のtop：' + aboutAreaHeight);
+
+   // 2. スクロールイベント時
+   $(window).on('scroll', function(){
+       // 3. スクロール値を変数dyに保存
+       var dy = $(this).scrollTop();
+       console.log(dy);
+      //  var bgPosition = dy / 2;
+      //  console.log(bgPosition);
+
+       // 4. スクロール値よりheaderHeightの半分が大きい時（if文）
+       if(aboutAreaHeight- $(window).height() < dy)
+       {
+           // 5. header部分の動き(dyに任意の係数を掛け算)
+         //   $('.ss-parallax').css('background-position', 'center bottom -' + bgPosition + 'px');
+         
+           $('.box1').css('transform', 'translateY(' + dy * 0.15 + 'px)');
+
+           $('.box2').css('transform', 'translateY(' + dy * -0.15 + 'px)');
+
+         //   $('.aw-parallax').css('transform', 'translateY(' + dy * -0.02 + 'px)');
+       }
+       // 6. それ以外の時（else文）（スクロール値がheaderHeightの半分を超えた時）
+      //  else
+      //  {
+      //      // 7. play部分の動き(dyに任意の係数を掛け算)
+      //      $('.stego').css('transform', 'translateX(' + dy * 0.4 + 'px)');
+      //      $('.tirano').css('transform', 'translateX(' + dy * -0.4 + 'px)')
+      //  }
+   });
+
+
+   // // 1. headerタグの高さ取得(変数headerHeight)
+   // var ssawAreaHeight= $('.ss-parallax').offset().top;
+   // console.log('ssaw画像のtop：' + ssawAreaHeight);
+
+   // // 2. スクロールイベント時
+   // $(window).on('scroll', function(){
+   //     // 3. スクロール値を変数dyに保存
+   //     var dy = $(this).scrollTop();
+   //     console.log(dy);
+   //    //  var bgPosition = dy / 2;
+   //    //  console.log(bgPosition);
+
+   //     // 4. スクロール値よりheaderHeightの半分が大きい時（if文）
+   //     if(ssawAreaHeight- $(window).height() < dy)
+   //     {
+   //         // 5. header部分の動き(dyに任意の係数を掛け算)
+   //       //   $('.ss-parallax').css('background-position', 'center bottom -' + bgPosition + 'px');
+         
+   //         $('.ss-parallax').css('transform', 'translateY(' + dy * 0.05 + 'px)');
+
+   //         $('.aw-parallax').css('transform', 'translateY(' + dy * 0.05 + 'px)');
+
+   //       //   $('.aw-parallax').css('transform', 'translateY(' + dy * -0.02 + 'px)');
+   //     }
+   //     // 6. それ以外の時（else文）（スクロール値がheaderHeightの半分を超えた時）
+   //    //  else
+   //    //  {
+   //    //      // 7. play部分の動き(dyに任意の係数を掛け算)
+   //    //      $('.stego').css('transform', 'translateX(' + dy * 0.4 + 'px)');
+   //    //      $('.tirano').css('transform', 'translateX(' + dy * -0.4 + 'px)')
+   //    //  }
+   // });
 });
